e8b2f0a6dce0d5398ea53c3378e622f968584b62
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-11-18 20:10:44 +0700

    initial commit

---
+++ controllers.js
@@ -0,0 +1,196 @@
+const express = require('express');
+const morgan = require('morgan');
+const { users } = require('./model');
+const utils = require('./utils');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const { Server } = require("socket.io");
+
+const io = new Server();
+
+
+module.exports = {
+    register: async (req, res) => {
+        try {
+            const existingUser = await users.findFirst({
+                where: {
+                    email: req.body.email
+                }
+            });
+
+            if (existingUser) {
+                return res.status(400).json({
+                    error: 'Email already exists',
+                });
+            }
+
+            const newUser = await users.create({
+                data: {
+                    email: req.body.email,
+                    password: await utils.cryptPassword(req.body.password),
+                },
+            });
+
+            return res.render('reg-success', { user: newUser });
+        } catch (error) {
+            console.error(error);
+            return res.render('error', {
+                message: error.message || 'Internal Server Error',
+            });
+        }
+    },
+
+    resetPassword: async (req, res) => {
+        try {
+            const findUser = await users.findFirst({
+                where: {
+                    email: req.body.email
+                }
+            });
+
+            if (!findUser) {
+                return res.render('error');
+            }
+
+            const resetToken = crypto.randomBytes(20).toString('hex');
+
+            await users.update({
+                data: {
+                    resetPasswordToken: resetToken,
+                },
+                where: {
+                    id: findUser.id
+                }
+            });
+
+            const transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: 'delaputri811@gmail.com',
+                    pass: 'raia twqi yjmt dtof'
+                }
+            });
+
+            const email = req.body.email;
+            const htmlTemplate = `
+            <!DOCTYPE html>
+            <html lang="en">
+            
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Reset Password</title>
+                <style type="text/css">
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        font-family: 'Arial', sans-serif;
+                        background-color: #edf4f5;
+                        color: #000000;
+                    }
+        
+                    .container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                        background-color: #ffffff;
+                        border-radius: 10px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    }
+        
+                    h1 {
+                        text-align: center;
+                        color: #30b3f1;
+                    }
+        
+                    p {
+                        font-size: 16px;
+                        line-height: 1.5;
+                        margin-bottom: 20px;
+                    }
+        
+                    a {
+                        color: #0000ee;
+                        text-decoration: underline;
+                    }
+        
+                    a:hover {
+                        color: #ff4500;
+                    }
+                </style>
+            </head>
+            
+            <body>
+                <div class="container">
+                    <h1>🎉 Hii ${email}, Your Email<br />Was Successfully Reset 🎉</h1>
+                    <p>
+                        To set a new password, click the following link:
+                        <br />
+                        <a href="http://localhost:3000/set-password/${resetToken}">Click Here</a>
+                    </p>
+                </div>
+            </body>
+            
+            </html>
+            `;
+
+            const mailOptions = {
+                from: 'system@gmail.com',
+                to: req.body.email,
+                subject: "Reset Password",
+                html: htmlTemplate
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+                
+                transporter.close();
+            });
+
+        } catch (error) {
+            console.error(error);
+            return res.render('error', {
+                message: error.message || 'Internal Server Error',
+            });
+        }
+    },
+
+    setPassword: async (req, res) => {
+        try {
+            const findUser = await users.findFirst({
+                where: {
+                    resetPasswordToken: req.body.key
+                }
+            });
+
+            if (!findUser) {
+                return res.render('error');
+            }
+
+            await users.update({
+                data: {
+                    password: await utils.cryptPassword(req.body.password),
+                    resetPasswordToken: null
+                },
+                where: {
+                    id: findUser.id
+                }
+            });
+
+            io.emit('passwordSet', { userId: findUser.id });
+
+            return res.render('success');
+        } catch (error) {
+            console.error(error);
+            return res.render('error', {
+                message: error.message || 'Internal Server Error',
+            });
+        }
+    },
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const Sentry = require("@sentry/node");
+const { Server } = require("socket.io");
+const http = require('http');
+const {PORT = 3000, ENV, SENTRY_DSN} = process.env;
+
+const index = express();
+const server = http.createServer(index);
+const io = new Server(server);
+
+const bodyParser  = require('body-parser');
+
+Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ index }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    environment: ENV,
+  });
+
+index.use(morgan('combined'));
+index.set('views', './views');
+index.set('view engine', 'ejs');
+index.use(cors());
+index.use(express.json());
+index.use(bodyParser.urlencoded({extended:true})); 
+index.use(bodyParser.json());
+
+index.use(Sentry.Handlers.requestHandler());
+
+index.use(Sentry.Handlers.tracingHandler());
+
+index.get("/", (req, res) => {
+  return res.json({
+    status: true,
+    message: "Hello World!",
+    error: null,
+    data: null,
+  });
+});
+
+const router = require('./routers');
+index.use(router);
+
+index.use((req, res, next) => {
+    req.io = io;
+    next();
+  });
+  
+  io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+  });
+
+index.listen(PORT, () => console.log(`App is running at PORT ${PORT}`))
+
